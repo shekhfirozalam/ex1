1da2d8178e977cec5c47962fee17fd678a4f8edb
pojagoyal <pooja.rani@berylsystems.com>
2015-02-19 17:41:10 +0530

    first commit

---
+++ ex4.rb
@@ -0,0 +1,3 @@
+
+a=10
+puts "my nos is #{a}"
---
+++ instance_var.rb
@@ -0,0 +1,23 @@
+
+class Customer
+   def initialize(id, name, addr)
+      @cust_id=id
+      @cust_name=name
+      @cust_addr=addr
+   end
+   def display_details()
+      puts "Customer id #@cust_id"
+      puts "Customer name #@cust_name"
+      puts "Customer address #@cust_addr"
+    end
+end
+
+# Create Objects
+cust1=Customer.new("1", "John", "Wisdom Apartments, Ludhiya")
+cust2=Customer.new("2", "Poul", "New Empire road, Khandala")
+cust3=Customer.new("3","san","faridabad sec 11d,faridabad")
+
+# Call Methods
+cust1.display_details()
+cust2.display_details()
+cust3.display_details()
